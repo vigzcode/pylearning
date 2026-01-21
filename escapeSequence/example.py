a516fc48378a0ec7a1e2a2785d70e302a5f3af0b
vigz <vigzcode@gmail.com>
2026-01-21 06:51:10 +0530

    escapeSequence

---
+++ escapeSequence/example.py
@@ -0,0 +1,33 @@
+# Moves the cursor to the next line
+print("Hello\nWorld")
+
+
+# Adds a horizontal tab (spacing).
+print("Name\tAge")
+print("John\t25")
+
+# Prints a backslash
+print("C:\\Users\\Admin")
+
+#Used when the string is enclosed in single quotes.
+print('It\'s a sunny day')
+
+#Used when the string is enclosed in double quotes.
+print("He said, \"Hello!\"")
+
+#Moves cursor one position back.
+print("Hello\bWorld")  
+
+# Moves cursor to the beginning of the line
+print("Hello\rWorld")
+
+# Form feed - moves to the next page (used in printing)
+print("Hello\fWorld")
+
+# Vertical tab - moves the cursor down to the next tab stop
+print("Hello\vWorld")
+
+print("\u2764")  # Heart symbol
+
+# If you don’t want escape sequences to be processed, use raw strings.
+print(r"C:\Users\Admin")
---
+++ escapeSequence/intro.md
@@ -0,0 +1,16 @@
+An escape sequence is a special character combination that starts with a backslash (\n).
+
+It is used to:
+
+    Add special characters in strings
+    Control formatting (new line, tab, etc.)
+    Insert characters that are otherwise hard to type
+
+
+Why Do We Need Escape Sequences?
+    Some characters have special meaning or are not visible, like:
+        New line
+        Tab space
+        Qoutes inside strings
+
+basically, Escape sequences help us represent these characters inside a string.
